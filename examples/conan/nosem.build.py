cb616248277977f6bdb52cf7fa524e1c0770848c
Sylvain Garcia <garcia.6l20@gmail.com>
2021-06-05 13:45:44 +0200

    conan test improvement

--- examples/conan/nosem.build.py
+++ examples/conan/nosem.build.py
@@ -1,13 +1,18 @@
-from nosem import project, executable, dependency, run_command, current_source_dir, current_build_dir, error, test
+from nosem import (project, executable, dependency, find_program, run_command, current_source_dir, current_build_dir, \
+                   error, test)
 
 project('conan')
 
-build_dir = current_build_dir()
-source_dir = current_source_dir()
-result = run_command('conan', 'install', source_dir, '-if', build_dir)
-if result.returncode != 0:
-    error(f'conan error: {result.stderr}')
+conan = find_program('conan', required=False)
+if not conan.found():
+    print(f'conan not found, conan-demo skipped')
+else:
+    build_dir = current_build_dir()
+    source_dir = current_source_dir()
+    result = run_command(conan.path(), 'install', source_dir, '-if', build_dir)
+    if result.returncode != 0:
+        error(f'conan error: {result.stderr}')
 
-catch2 = dependency('catch2', required=True, extra_path=build_dir)
-demo = executable('conan-demo', 'demo.cpp', dependencies=catch2)
-test('conan-demo', demo)
+    catch2 = dependency('catch2', required=True, extra_path=build_dir)
+    demo = executable('conan-demo', 'demo.cpp', dependencies=catch2)
+    test('conan-demo', demo)
